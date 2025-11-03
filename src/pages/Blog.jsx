53c8fe60383de4a87b8bfad27900c7b98108e549
bluesdriver <98720261+bluesdriver@users.noreply.github.com>
2025-11-03 21:46:23 +0000

    chore: search engine optimization

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,7 +2,7 @@ import { PostList } from '../components/PostList.jsx'
 import { CreatePost } from '../components/CreatePost.jsx'
 import { PostFilter } from '../components/PostFilter.jsx'
 import { PostSorting } from '../components/PostSorting.jsx'
-
+import { Helmet } from 'react-helmet-async'
 import { Header } from '../components/Header.jsx'
 
 import { useState } from 'react'
@@ -24,6 +24,9 @@ export function Blog() {
 
   return (
     <div style={{ padding: 8 }}>
+      <Helmet>
+        <title>Full-Stack React Blog</title>
+      </Helmet>
       <Header />
       <br />
       <hr />
